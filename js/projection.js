9750df541bb78718820ec75e728e2cb71acdcffe
Gianluca Truda <gianlucatruda@gmail.com>
2024-05-02 19:28:40 +0100

    Fixed rounding bug in definePixel. Works!

--- js/projection.js
+++ js/projection.js
@@ -3,8 +3,6 @@ var ctx = canvas.getContext('2d');
 console.log("Clearing canvas...");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-console.log(data)
-
 const sceneDims = [500, 500, 500];
 const F = 200; // Focal length
 const camPos = [200, 150, 0];
@@ -24,7 +22,9 @@ function drawPixel(x, y, color) {
 }
 
 function definePixel(x, y, [r, g, b, a]) {
-    let d = Math.round((y * canvas.width + x) * 4);
+    let _x = Math.round(x)
+    let _y = Math.round(y);
+    let d = (_y * canvas.width + _x) * 4;
     data.data[d + 0] = r;
     data.data[d + 1] = g;
     data.data[d + 2] = b;
@@ -53,9 +53,23 @@ function fill3DBox(startX, startY, startZ, endX, endY, endZ) {
         }
     }
 }
-
 console.log("Creating a box...");
-fill3DBox(200, 200, 200, 300, 300, 201);
+fill3DBox(200, 200, 200, 300, 300, 300);
+
+function fillSphere(x0, y0, z0, r) {
+    for (let x = 0; x < sceneDims[0]; x++) {
+        for (let y = 0; y < sceneDims[1]; y++) {
+            for (let z = 0; z < sceneDims[2]; z++) {
+                if ((x - x0) ** 2 + (y - y0) ** 2 + (z - z0) ** 2 == r ** 2) {
+                    scene3D[x][y][z] = 1;
+                }
+            }
+        }
+    }
+}
+
+// console.log("Creating a sphere...");
+// fillSphere(100, 100, 300, 1j0);
 
 function projectScene() {
 
@@ -96,8 +110,7 @@ function projectScene() {
     let newImgData = new ImageData(data.data, canvas.width, canvas.height);
     ctx.putImageData(newImgData, 0, 0);
     console.log(data.data)
-    console.log("Done?")
-
+    console.log("Done")
 }
 
 projectScene();
