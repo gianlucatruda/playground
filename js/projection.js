d5d43617d4f46042db4313bb68f5065623a615d2
Gianluca Truda <gianlucatruda@gmail.com>
2024-05-02 20:16:29 +0100

    Projection: hollow shapes, naive z shading. Looks decent.

--- js/projection.js
+++ js/projection.js
@@ -4,7 +4,7 @@ console.log("Clearing canvas...");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 var data = ctx.getImageData(0, 0, canvas.width, canvas.height);
 const sceneDims = [500, 500, 500];
-const F = 200; // Focal length
+const F = 190; // Focal length
 const camPos = [200, 150, 0];
 
 function fillSphere(x0, y0, z0, r) {
@@ -12,9 +12,9 @@ function fillSphere(x0, y0, z0, r) {
     for (let x = 0; x < sceneDims[0]; x++) {
         for (let y = 0; y < sceneDims[1]; y++) {
             for (let z = 0; z < sceneDims[2]; z++) {
-                if ((x - x0) ** 2 + (y - y0) ** 2 + (z - z0) ** 2 <= r ** 2) {
+                if ((x - x0) ** 2 + (y - y0) ** 2 + (z - z0) ** 2 - r ** 2 <= 0.001) {
                     sphereCount++;
-                    scene3D[x][y][z] = 1;
+                    scene3D[x][y][z] = 255;
                 }
             }
         }
@@ -26,7 +26,9 @@ function fill3DBox(startX, startY, startZ, endX, endY, endZ) {
     for (let x = startX; x < endX; x++) {
         for (let y = startY; y < endY; y++) {
             for (let z = startZ; z < endZ; z++) {
-                scene3D[x][y][z] = 1;
+                if (x == startX | x == endX | y == startY | y == endY | z == startZ | z == endZ) {
+                    scene3D[x][y][z] = 255;
+                }
             }
         }
     }
@@ -42,6 +44,11 @@ function definePixel(x, y, [r, g, b, a]) {
     data.data[d + 3] = a;
 }
 
+function drawScene() {
+    let newImgData = new ImageData(data.data, canvas.width, canvas.height);
+    ctx.putImageData(newImgData, 0, 0);
+}
+
 function projectScene() {
     console.log("Clearing canvas...");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -50,8 +57,8 @@ function projectScene() {
     let dCount = 0;
     for (let x = 0; x < sceneDims[0]; x++) {
         for (let y = 0; y < sceneDims[1]; y++) {
-            for (let z = sceneDims[2]; z > 1; z--) {
-                if (scene3D[x][y][z] == 1) {
+            for (let z = 0; z < sceneDims[2]; z++) {
+                if (scene3D[x][y][z] > 0) {
                     // Offset camera position in scene
                     let dx = x - camPos[0];
                     let dy = y - camPos[1];
@@ -61,7 +68,9 @@ function projectScene() {
                     // 2D projection
                     let x2D = dx * (F / dz) + (canvas.width / 2);
                     let y2D = dy * (F / dz) + (canvas.height / 2);
-                    let c = [128, 128, 128, 100];
+                    // let C = 255 * (1 - dz / sceneDims[2]);
+                    let C = scene3D[x][y][z] * (1 - dz / sceneDims[2]);
+                    let c = [C, C, C, 255];
                     definePixel(x2D, y2D, c);
                     dCount++;
                 }
@@ -75,15 +84,14 @@ function projectScene() {
             definePixel(x, y, [255, 0, 0, 255]);
         }
     }
-    let newImgData = new ImageData(data.data, canvas.width, canvas.height);
-    ctx.putImageData(newImgData, 0, 0);
+    drawScene();
     console.log("Done")
 }
 
 console.log(`Creating a ${sceneDims[0]} x ${sceneDims[1]} x ${sceneDims[2]} scene...`);
 var scene3D = Array(sceneDims[0]).fill().map(() => Array(sceneDims[1]).fill().map(() => Array(sceneDims[2]).fill(0)));
 console.log("Creating a box...");
-fill3DBox(200, 200, 200, 300, 300, 300);
+fill3DBox(210, 190, 199, 210 + 101, 190 + 101, 199 + 101);
 console.log("Creating a sphere...");
 fillSphere(100, 100, 300, 50);
 
