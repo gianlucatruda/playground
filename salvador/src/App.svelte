69f5316917c5ef653f6b604c83209e82bacd0929
Gianluca Truda <gianlucatruda@gmail.com>
2024-07-11 18:29:52 +0100

    Initial version of Salvador. Custom Dalle frontend.

--- salvador/src/App.svelte
+++ salvador/src/App.svelte
@@ -1,24 +1,80 @@
 <script>
-	export let name;
+	let btnText = "Generate Image";
+	let promptText = "A melting clock in the style of Dali.";
+	let btnDisabled = false;
+	let imgURL = "";
+
+	const makeImage = () => {
+		console.log("Querying:", promptText);
+		btnDisabled = true;
+		btnText = "Painting...";
+		makeRequest(promptText);
+
+	};
+
+	async function makeRequest(p){
+
+        const url = "https://api.openai.com/v1/images/generations";
+        const bodyData = {
+            model: "dall-e-2",
+            // model: "dall-e-3",
+            prompt: p,
+            n: 1,
+            size: "256x256",
+            // size: "1024x1024"
+        };
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${OPENAI_API_KEY}`
+                },
+                body: JSON.stringify(bodyData)
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const result = await response.json();
+            console.log(result.data[0]);
+			imgURL = result.data[0].url;
+
+        } catch (error) {
+            console.error('Failed to fetch image:', error);
+        }
+	}
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Salvador</h1>
+	<p>A custom frontend for <a href="https://openai.com/index/dall-e-3/">OpenAI's DALLE 3</a></p>
+	<div class="modelParams">
+		<label for="prompt">Prompt</label>
+		<textarea id="prompt" rows="3" cols="40" wrap="soft" bind:value={promptText}></textarea>
+		<button on:click={makeImage} disabled={btnDisabled}>{btnText}</button>
+	</div>
+	<div class="results">
+		{#if imgURL !== ""}
+			<img src={imgURL} alt="result">
+		{/if}
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
+		padding: 0.5em;
+		max-width: 600px;
 		margin: 0 auto;
 	}
 
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
-		font-size: 4em;
+		font-size: 3em;
 		font-weight: 100;
 	}
 
