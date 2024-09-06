057e1fa1650fa75200e879833a6b706ede86e348
Gianluca Truda <dev@gianluca.ai>
2024-09-06 18:53:43 +0100

    First lib test and failing fn.

--- rust/minigrep/src/lib.rs
+++ rust/minigrep/src/lib.rs
@@ -22,6 +22,28 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // Box<dyn Error> means the function will return a type that implements the Error trait
     // This gives us flexibility to return error values that may be of different types in different error cases.
     let contents = fs::read_to_string(config.file_path)?;
-    dbg!(&contents);
     Ok(()) // idiomatic way to indicate that we’re calling run for its side effects only
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    // Because contents is the argument that contains all of our text and 
+    // we want to return the parts of that text that match, 
+    // we know contents is the argument that should be connected to the 
+    // return value using the lifetime syntax.
+    return vec![];
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
