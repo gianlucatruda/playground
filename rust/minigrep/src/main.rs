f1912dbfef1cc05fb0d9cfcd92598de9f6be57c4
Gianluca Truda <dev@gianluca.ai>
2024-09-06 20:08:29 +0100

    Stderror feature

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -13,15 +13,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    // Because run returns () in the success case, we only care about detecting an error, 
+    // Because run returns () in the success case, we only care about detecting an error,
     // so we don’t need unwrap_or_else to return the unwrapped value
     if let Err(e) = minigrep::run(config) {
-        println!("Problem running: {e}");
+        eprintln!("Problem running: {e}");
         process::exit(1);
     }
 }
-
