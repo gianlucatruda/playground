e4fe0960709b66ad38d214ab71ee6288819cfaa0
Gianluca Truda <dev@gianluca.ai>
2024-09-06 17:43:45 +0100

    Refactor main.rs

--- rust/minigrep/src/main.rs
+++ rust/minigrep/src/main.rs
@@ -11,13 +11,24 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
+    dbg!(&config);
 
-    dbg!(query, file_path);
+    let contents = fs::read_to_string(config.file_path).expect("Unable to read file");
+    dbg!(&contents);
+}
 
-    let contents = fs::read_to_string(file_path)
-        .expect("Unable to read file");
-    dbg!(contents);
+#[derive(Debug)]
+struct Config {
+    query: String,
+    file_path: String,
+}
 
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config {
+        query: query.to_string(),
+        file_path: file_path.to_string(),
+    }
 }
